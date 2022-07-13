b116182a2282cdb119515067a985dfe428d56cb6
Nelson-Cod <nelsonorogwu@gmail.com>
2022-07-13 07:53:19 -0700

    task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -23,13 +23,13 @@ for (x = 0; x < 100; x++)
 pass[x] = rand() % 78;
 sum += (pass[x] + '0');
 putchar(pass[x] + '0');
-if ((2772 - sum) -'0' < 78)
+if ((2772 - sum) - '0' < 78)
 {
 y = 2772 - sum - '0';
 sum += y;
 putchar(y + '0');
 break;
 }
-return (0);
 }
+return (0);
 }
